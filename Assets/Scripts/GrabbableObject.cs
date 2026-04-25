8adf5ed8d699d67c3d6b318c0138e7b8f13c4e57
Michael Wetherill <michael.wetherill@gmail.com>
2026-04-25 19:30:04 -0400

    Setup some code I will need for "breaking" objects.  (They will just grey out)

--- Assets/Scripts/GrabbableObject.cs
+++ Assets/Scripts/GrabbableObject.cs
@@ -1,7 +1,56 @@
+using System;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Rigidbody))]
 public class GrabbableObject : MonoBehaviour
 {
-    public Rigidbody rb;
+    [Header("Physics")]
+    public Rigidbody rigidBody;
+
+    public float breakForce = 1f;
+    public int maxCreatureCount = 8;
+    public GameObject breakVFXPrefab;
+    public AudioClip breakSFX;
+    public AudioClip[] impactSFX;
+
+    [NonSerialized] public bool isBroken;
+
+    [SerializeField] private MeshRenderer[] meshRenderers; 
+    
+    private void OnValidate()
+    {
+        //So I don't have to deal with assigning these
+        meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        if (meshRenderers.Length == 0)
+        {
+            Debug.LogWarning($"{this.name} was unable to find any mesh renderers" );
+        }
+
+        if (rigidBody == null)
+        {
+            rigidBody = GetComponent<Rigidbody>();
+            if (rigidBody == null)
+                Debug.LogWarning($"{this.name} is missing a rigidbody component");
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isBroken)
+            return;
+
+        float impact = collision.relativeVelocity.magnitude * rigidBody.mass;
+
+        var audioClip = impactSFX[Random.Range(0, impactSFX.Length)];
+        
+    }
+
+    private void Break()
+    {
+        
+    }
 }
